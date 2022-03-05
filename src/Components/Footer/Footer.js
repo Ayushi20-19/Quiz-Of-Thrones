9fede8978e8276cdf650c21b659194852644c678
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2022-03-05 13:50:46 +0530

    add footer section

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const getYear = () => new Date().getFullYear();
+  return (
+    <>
+      <footer>
+        <div className='footer-content'>
+          <div>
+            <h3 className='footer-heading'>Quiz Of Thrones</h3>
+          </div>
+          <div className='footer-links'>
+            <span>About Us</span>
+            <span>Categories</span>
+            <span>Contact Us</span>
+            <span>Privacy Policy</span>
+          </div>
+          <div className='footer-copyright'>
+            Copyright © {getYear()} Quiz Of Throne. All Rights Reserved
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import FeatureCards from "./FeatureCards";
 import HeroSection from "./HeroSection";
-
+import Footer from "../Footer/Footer";
 const HomePage = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
       <FeatureCards />
+      <Footer />
     </div>
   );
 };
